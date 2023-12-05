4560ca2c6825d5a7260450b41364c59672f0afb5
Diavlo071 <jmonjo20713@iesjoanramis.org>
2023-12-05 12:15:38 +0100

    Corregit el exercici 21

--- ex21.py
+++ ex21.py
@@ -1,11 +1,11 @@
 def crear_repetits(a,b):
-    c = b
+    c = ''
     for i in range(a):
-        b = b + c
-    return b
+        c += b
+    return c
 
 #Programa principal
 
 numero = int(input("Inserta el numero: "))
-caracter = input("Inserta el caracter: ")
+caracter = str(input("Inserta el caracter: "))
 print(crear_repetits(numero, caracter))
