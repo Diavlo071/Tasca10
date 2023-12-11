33bc3fa31127e8234d0f69714821d5accfec9b25
Diavlo071 <jmonjo20713@iesjoanramis.org>
2023-12-11 10:21:39 +0100

    exercici 14 corregit

--- ex14.py
+++ ex14.py
@@ -1,16 +1,23 @@
-def gran_de_tres():
+def gran_de_tres(a,b,c):
 
-    if a > b and a > c:    
+    if a > b > c:    
         print("El nombre {} es major que {} i {}".format(a,b,c))
     
-    elif b > a and b > c:
+    elif a > c > b:
+        print("El nombre {} es major que {} i {}".format(a,c,b))
+    elif b > c > a:
+        print("El nombre {} es major que {} i {}".format(b,c,a))
+    elif b > a > c:
         print("El nombre {} es major que {} i {}".format(b,a,c))
-        
-    else:
+    elif c > a > b:
         print("El nombre {} es major que {} i {}".format(c,a,b))
-        
-    
 
+    elif c > b > a:
+        print("El nombre {} es major que {} i {}".format(c,b,a))
+
+    else:
+        print(a)
+        
 
 #Programa principal
 
@@ -20,4 +27,4 @@ b = int(input("Introdueix el Segon nombra: "))
 
 c = int(input("Introdueix el Tercer nombra: "))
 
-gran_de_tres()
+gran_de_tres(a,b,c)
